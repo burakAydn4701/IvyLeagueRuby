7deebc81b2aa16ff389246b7f39d505705691e3b
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2025-02-12 22:07:10 +0300

    dropped zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module IvyLeagueRuby
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    # Add this line to disable Zeitwerk mode
+    config.autoloader = :classic
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
